4a24ec018c5fd71c355cede0ee51fbfb68687093
Aarti khankul <62983983+Aarti3530@users.noreply.github.com>
2021-02-07 15:09:01 +0530

    Add files via upload

---
+++ SmallestNumInUnsortedArray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[20],n,item;
+	cin>>n;
+	for(int i = 1;i<=n;i++){
+		cin>>arr[i];
+	}
+	item = arr[1];
+	for(int i = 2;i<=n;i++){
+		if(arr[i] < item )
+		{
+			item = arr[i];
+		}
+	}
+	cout<<"Smallest num is "<<item;
+}

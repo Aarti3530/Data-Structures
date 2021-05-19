fab8471df848c613b9f2abb279940504349359f5
Aarti khankul <62983983+Aarti3530@users.noreply.github.com>
2021-05-19 15:34:11 +0530

    Create UniqueNumbers.java

---
+++ UniqueNumbers.java
@@ -0,0 +1,51 @@
+/Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            
+            
+            int l=sc.nextInt();
+			int r=sc.nextInt();
+			Solution ob = new Solution();
+			ArrayList<Integer> numbers = ob.uniqueNumbers(l,r);
+			for(int num : numbers){
+			    System.out.print(num+" ");
+			}
+			System.out.println();
+
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution{
+
+    static ArrayList<Integer> uniqueNumbers(int L,int R){
+        
+       ArrayList<Integer> a = new ArrayList<Integer>();    
+       for(int i=L;i<=R;i++){
+           
+           String s = ""+i;
+           HashSet<Character> set = new HashSet<Character>();
+           for(int j=0;j<s.length();j++){
+           char ch = s.charAt(j); // unique elements
+           set.add(ch);
+               
+           }
+           
+           if(s.length() == set.size()){
+               a.add(i);
+           }
+       }
+       return a;
+    }
+}

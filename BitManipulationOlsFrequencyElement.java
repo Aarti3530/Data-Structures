f614b7d6d5f90f60f36811118e35e675c9e1d222
Aarti khankul <62983983+Aarti3530@users.noreply.github.com>
2021-05-31 13:39:56 +0530

    Create BitManipulationOlsFrequencyElement.java

---
+++ BitManipulationOlsFrequencyElement.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class CandidateCode {
+   public static void main(String args[] ) throws Exception {
+
+	Scanner sc = new Scanner(System.in);
+   int n = sc.nextInt();
+   int [] arr = new int[n];
+
+   for(int i=0;i<n;i++)
+   arr[i] = sc.nextInt();
+
+   int t = arr[0];
+   for(int i=1;i<n;i++)
+   t = t^arr[i];
+
+   System.out.println(t);
+
+   }
+}

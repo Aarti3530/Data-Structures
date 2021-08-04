a59a8d715c1c075ad10e91feb52ef2afb90329eb
Aarti khankul <62983983+Aarti3530@users.noreply.github.com>
2021-08-04 14:28:07 +0530

    Update Output names that starts with vowel.sql

--- Output names that starts with vowel.sql
+++ Output names that starts with vowel.sql
@@ -1,1 +1,6 @@
 SELECT DISTINCT city FROM station WHERE city REGEXP "^[aeiou].*";
+
+for first and last word sholed ne vowel
+
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY REGEXP '^[aeiou].*[aeiou]$';
